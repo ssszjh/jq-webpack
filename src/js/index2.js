e7b57bd679f9254aafcda0fdf120457f87049deb
YBAU <478176546@qq.com>
2020-11-09 16:49:22 +0800

    1

--- src/js/index2.js
+++ src/js/index2.js
@@ -1,30 +1,3 @@
 import '../css/index2.css';
 import '../common/common.js';
 console.log(2);
-
-
-layui.use('table', function() {
-    var table = layui.table;
-
-    // //第一个实例
-    // table.render({
-    //     elem: '#demo',
-    //     height: 312,
-    //     url: '/demo/table/user/',
-    //     page: true,
-    //     cols: [
-    //         [
-    //             { field: 'id', title: 'ID', width: 80, sort: true, fixed: 'left' },
-    //             { field: 'username', title: '用户名', width: 80 },
-    //             { field: 'sex', title: '性别', width: 80, sort: true },
-    //             { field: 'city', title: '城市', width: 80 },
-    //             { field: 'sign', title: '签名', width: 177 },
-    //             { field: 'experience', title: '积分', width: 80, sort: true },
-    //             { field: 'score', title: '评分', width: 80, sort: true },
-    //             { field: 'classify', title: '职业', width: 80 },
-    //             { field: 'wealth', title: '财富', width: 135, sort: true }
-    //         ]
-    //     ]
-    // });
-
-});
